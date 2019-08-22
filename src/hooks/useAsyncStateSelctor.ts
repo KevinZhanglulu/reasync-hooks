9fbb24a1a0a47b38ae2cef9c73a6e92339e4eabf
Kevin Zhang <kevinzhanglulu@gmail.com>
2019-08-22 19:44:11 +0800

    fix: unnecessary re-render when async action state changes from fulfilled or rejected to pending

--- src/hooks/useAsyncStateSelctor.ts
+++ src/hooks/useAsyncStateSelctor.ts
@@ -1,46 +1,49 @@
-import { useEffect, useReducer, useRef } from "react";
-import { shallowEqual, useSelector } from "react-redux";
+import { useRef } from "react";
+import { useSelector } from "react-redux";
+
+type ActionState = undefined | string;
 
 export const useAsyncStateSelector = (
   actionTypes: string[],
   asyncStateType: string,
   asyncStateReducerKey: string
 ) => {
-  const [, forceRender] = useReducer(s => s + 1, 0);
-  const currentAsyncStates = useRef<(string | undefined)[]>();
-
   const asyncAction = useRef<{ type: string | null }>({
     type: null
   });
 
   const asyncStateSelector = (state: any) => {
-    return actionTypes.map(actionName => {
+    return actionTypes.map(actionType => {
       if (!state[asyncStateReducerKey])
         throw new Error(
           `You may not pass {${asyncStateReducerKey}:asyncStateReducer} to combineReducers()`
         );
-      return state[asyncStateReducerKey][actionName];
+      return state[asyncStateReducerKey][actionType];
     });
   };
 
-  const nextStates = useSelector<any, (string | undefined)[]>(
-    state => asyncStateSelector(state),
-    shallowEqual
-  );
-
-  useEffect(() => {
-    if (!currentAsyncStates.current) currentAsyncStates.current = nextStates;
-    const length = currentAsyncStates.current.length;
-    for (let i = 0; i < length; i++) {
-      if (nextStates[i] && currentAsyncStates.current[i] !== nextStates[i]) {
-        currentAsyncStates.current = nextStates;
-        if (nextStates[i] === asyncStateType) {
+  const equalityFn = (
+    newStates: ActionState[],
+    currentStates: ActionState[]
+  ) => {
+    for (let i = 0; i < currentStates.length; i++) {
+      if (currentStates[i] !== newStates[i]) {
+        currentStates[i] = newStates[i];
+        if (newStates[i] === asyncStateType) {
           asyncAction.current = { type: actionTypes[i] };
-          forceRender({});
+          //Re-render
+          return false;
         }
       }
     }
-  }, [nextStates]);
+    return true;
+  };
+
+  useSelector<any, ActionState[]>(
+    state => asyncStateSelector(state),
+    //The useSelector calls equalityFn in the useEffect on the server or useLayoutEffect on the browser.(https://github.com/reduxjs/react-redux/blob/0c5f7646f600e635e1caf62863ad61350011f3e7/src/hooks/useSelector.js#L71)
+    equalityFn
+  );
 
   return asyncAction.current;
 };
