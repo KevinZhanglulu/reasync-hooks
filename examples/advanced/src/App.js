70b82a37b7b2e33e62404e1aa2cc9925eacc5c8c
Kevin Zhang <kevinzhanglulu@gmail.com>
2019-08-23 23:46:57 +0800

    docs: fix css bug for examples

--- examples/advanced/src/App.js
+++ examples/advanced/src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
 import("antd/dist/antd.css");
+import ("./App.css");
+import React from "react";
 import {
   applyMiddleware,
   combineReducers,
--- examples/basis/src/App.js
+++ examples/basis/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "./App.css";
 import("antd/dist/antd.css");
+import "./App.css";
+import React from "react";
 import { applyMiddleware, combineReducers, createStore ,compose} from "redux";
 import {
   asyncReduxMiddlewareCreator,
